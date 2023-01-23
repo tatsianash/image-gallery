0f758c090924a744dff83b9c92429a711066828b
Tatsiana Shapavalava <tatsiana_shapavalava1@epam.com>
2023-01-23 16:24:30 -0700

    Fix configurations

--- postcss.config.js
+++
@@ -1,5 +0,0 @@
-const tailwindcss = require('tailwindcss');
-
-module.exports = {
-  plugins: [tailwindcss('./tailwind.css'), require('autoprefixer')],
-};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.ts*', './**/*.html'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+
   theme: {
     extend: {},
   },
